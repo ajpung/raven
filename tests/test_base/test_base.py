77168edd775193fec02a1ea3d37535c00ae4162d
ajpung <83614749+ajpung@users.noreply.github.com>
2025-02-27 07:27:57 -0500

    chore: adding conversions for dates and times to tz-aware datetimes and epoch

--- tests/test_base/test_base.py
+++ tests/test_base/test_base.py
@@ -1,2 +1,39 @@
-def test_placeholder() -> None:
-    assert True
+import datetime
+from raven.core.date_time import correct_timezone
+from raven.core.date_time import datetime_to_epoch
+
+
+def test_dt_convert():
+    date_string = "February 26, 2025, 12:00 PM"
+    datetime_object = correct_timezone(date_string)
+    assert datetime_object.tzinfo == datetime.timezone.utc
+    assert datetime_object == datetime.datetime(
+        2025, 2, 26, 12, 0, tzinfo=datetime.timezone.utc
+    )
+    date_string = "2025-02-26T12:00:0Z"
+    datetime_object = correct_timezone(date_string)
+    assert datetime_object.tzinfo == datetime.timezone.utc
+    assert datetime_object == datetime.datetime(
+        2025, 2, 26, 12, 0, tzinfo=datetime.timezone.utc
+    )
+    date_string = "February 26, 2025, 7:00AM EST"
+    datetime_object = correct_timezone(date_string)
+    assert datetime_object.tzinfo == datetime.timezone.utc
+    assert datetime_object == datetime.datetime(
+        2025, 2, 26, 12, 0, tzinfo=datetime.timezone.utc
+    )
+
+
+def test_calculate_utc():
+    date_string = "February 26, 2025, 12:00 PM"
+    datetime_object = correct_timezone(date_string)
+    datetime_epoch = datetime_to_epoch(datetime_object)
+    assert datetime_epoch == 1740571200
+    date_string = "2025-02-26T12:00:0Z"
+    datetime_object = correct_timezone(date_string)
+    datetime_epoch = datetime_to_epoch(datetime_object)
+    assert datetime_epoch == 1740571200
+    date_string = "February 26, 2025, 7:00AM EST"
+    datetime_object = correct_timezone(date_string)
+    datetime_epoch = datetime_to_epoch(datetime_object)
+    assert datetime_epoch == 1740571200
