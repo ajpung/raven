bc994bb00536a5dfc9322d49552dac1b77ef1d79
ajpung <83614749+ajpung@users.noreply.github.com>
2025-03-03 07:30:26 -0500

    .

--- src/raven/modules/weather/open_meteo.py
+++ src/raven/modules/weather/open_meteo.py
@@ -446,7 +446,9 @@ def fill_openmeteo(
     ] + [current.Variables(i).Value() for i in range(122, 141)]
     openmet_dict["data"]["wind"]["gust"]["heights"] = [10]
     openmet_dict["data"]["wind"]["gust"]["values"] = [current.Variables(29).Value()]
-    openmet_dict["data"]["wind"]["direction"]["heights"] = wind_altitudes_km + altitudes_km
+    openmet_dict["data"]["wind"]["direction"]["heights"] = (
+        wind_altitudes_km + altitudes_km
+    )
     openmet_dict["data"]["wind"]["direction"]["values"] = [
         current.Variables(i).Value() for i in range(25, 29)
     ] + [current.Variables(i).Value() for i in range(141, 160)]
