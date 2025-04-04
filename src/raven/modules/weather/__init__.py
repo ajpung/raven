92a9b134189a113e9ec64872d3e86310f7b8735c
ajpung <83614749+ajpung@users.noreply.github.com>
2025-04-04 07:33:00 -0400

    .

--- src/raven/modules/weather/__init__.py
+++ src/raven/modules/weather/__init__.py
@@ -1,64 +1,13 @@
-from .open_meteo import (
-    # gather_openmeteo,
-    # correct_openmeteo,
-    # fill_openmeteo,
-    collect_openmeteo,
-)
-from .tomorrow_io import (
-    # gather_tomorrow,
-    # correct_tomorrow,
-    # fill_tomorrow,
-    collect_tomorrow,
-)
-from .visual_crossing import (
-    # gather_visualcrossing,
-    # correct_visualcrossing,
-    # fill_visualcrossing,
-    collect_visualcrossing,
-)
-
-from .synoptic_data import gather_synoptic
-
-from .accu_weather import (
-    # gather_accuwx,
-    # gather_location,
-    # correct_accuwx,
-    # fill_accuwx,
-    collect_accuwx,
-)
-
-from .aviation_wx import (
-    # gather_aviation_wx,
-    # correct_aviation,
-    # fill_aviation,
-    collect_aviationwx,
-)
+from .open_meteo import collect_openmeteo
+from .tomorrow_io import collect_tomorrow
+from .visual_crossing import collect_visualcrossing
+from .accu_weather import collect_accuwx
+from .aviation_wx import collect_aviationwx
 
 __all__ = [
-    # "gather_tomorrow",
-    # "correct_tomorrow",
-    # "fill_tomorrow",
     "collect_tomorrow",
-    # "gather_openmeteo",
-    # "correct_openmeteo",
-    # "fill_openmeteo",
     "collect_openmeteo",
-    # "gather_visualcrossing",
-    # "correct_visualcrossing",
-    # "fill_visualcrossing",
     "collect_visualcrossing",
-    "gather_synoptic",
-    # "gather_location",
-    # "gather_accuwx",
-    # "correct_accuwx",
-    # "fill_accuwx",
     "collect_accuwx",
-    # "gather_aviation_wx",
-    # "correct_aviation",
-    # "fill_aviation",
     "collect_aviationwx",
-    # "gather_meteostat",
-    # "correct_meteostat",
-    # "fill_meteostat",
-    "collect_meteostat",
 ]
